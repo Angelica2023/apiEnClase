0b904999f88314335b90db0ac156624c09b2d909
Norma Angelica <angelica.alp@gmail.com>
2023-05-03 10:30:02 -0600

    ultima modificacion 3-05 10:29 am

--- api.js
+++ api.js
@@ -6,11 +6,7 @@ import usuarios from "./usuarios.json" assert { type: "json" };
   
     setTimeout(() => {
 
-//var allData = usuarios.slice(); // Copia todos los elementos del arreglo original
-
-  //  console.log(allData); // Muestra el arreglo completo en la consola
-
-    if (usuarios.length > 0) {
+ if (usuarios.length > 0) {
         // El arreglo no está vacío
         resolve("Los usuarios son:",usuarios);
       } else {
