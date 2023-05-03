114efeb669103089a90d9206e7c7c2e97218f6c3
Norma Angelica <angelica.alp@gmail.com>
2023-05-02 23:13:08 -0600

    avance al 2 mayo 23.12 hrs

--- api.js
+++ api.js
@@ -1,1 +1,25 @@
-import usuarios from 'usuarios.json' assert { type: "json" };
+import usuarios from "./usuarios.json" assert { type: "json" };
+
+ // console.log (usuarios)
+
+  const promesaPorCumplir = new Promise((resolve, reject) => {
+  
+    setTimeout(() => {
+
+//var allData = usuarios.slice(); // Copia todos los elementos del arreglo original
+
+  //  console.log(allData); // Muestra el arreglo completo en la consola
+
+    if (usuarios.length > 0) {
+        // El arreglo no está vacío
+        resolve("Los usuarios son:",usuarios);
+      } else {
+        // El arreglo está vacío
+       reject("ERROR al buscar usuarios");
+      }
+    }, 4000);
+  });
+  
+  promesaPorCumplir
+    .then((respuesta) => console.log(usuarios))
+    .catch((error) => console.log(error));
